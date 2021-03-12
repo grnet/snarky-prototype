2372cffcdf8c9dcf4ac8fd14c0d7789a01391c80
fmerg <fmerg@grnet.gr>
2021-03-12 21:18:39 +0200

    (setup): Some clenup

--- demo.py
+++ demo.py
@@ -10,11 +10,7 @@ class Verifier(object):
 
 from collections import namedtuple
 
-Tau = namedtuple('Tau', ['alpha', 'beta', 'delta', 'x'])   # τ = (α, β, δ, x) E (Z_p)*
-
-def extract_tau(tau):
-    return tau.alpha, tau.beta, tau.delta, tau.x
-
+Trapdoor = namedtuple('Trapdoor', ['alpha', 'beta', 'delta', 'x'])   # τ = (α, β, δ, x) E (Z_p)*
 
 class Poly(object):
 
@@ -30,7 +26,10 @@ class Poly(object):
 
     def eval(self, x):
         return sum(
-            map(lambda i: self.coeffs[i] * x ** i, range(0, len(self.coeffs))), 
+            map(
+                lambda i: self.coeffs[i] * x ** i,      # TODO: mod p
+                range(0, len(self.coeffs))
+            ), 
             bnZero()
         )
 
@@ -56,14 +55,34 @@ class QAP(object):
         self.v = v
         self.w = w
 
+    @classmethod
+    def create_default(cls, ctx, m, n, l):
+        u = [Poly([1] + (n - 1) * [0]) for _ in range(0, m + 1)]
+        v = [Poly([1] + (n - 1) * [0]) for _ in range(0, m + 1)]
+        w = [Poly([1] + (n - 1) * [0]) for _ in range(0, m + 1)]
+        t = Poly([1] + n * [0])
+        return QAP(ctx, u, v, w, t, l)
 
-def generate_srs_u(ctx, tau, n):
+    def polynomials(self):
+        u = self.u
+        v = self.v
+        w = self.w
+        t = self.t
+        return u, v, w, t
+
+    def dimensions(self):
+        m = self.m
+        n = self.n
+        l = self.l
+        return m, n, l
+
+
+def generate_srs_u(ctx, trapdoor, qap):
+    _, G, H = ctx
+    alpha, beta, _, x = trapdoor
+    n = qap.n
+
     srs_u = [[], []]
-
-    G = ctx.G
-    H = ctx.H
-
-    alpha, beta, _, x = extract_tau(tau)
     for i in range(0, 2 * n -1):                # 0 <= i <= 2n - 2
         srs_u[0].append((
             (x ** i) * G,                       # (x ^ i) * G
@@ -78,23 +97,14 @@ def generate_srs_u(ctx, tau, n):
         ))
     return srs_u
 
-def generate_srs_s(ctx, tau, qap):
+
+def generate_srs_s(ctx, trapdoor, qap):
     assert ctx.p == qap.p
-    p = ctx.p
-    G = ctx.G
-    H = ctx.H
-    
 
-    alpha, beta, delta, x = extract_tau(tau)
-
-    # TODO: Implement and apply QAP extraction
-    u = qap.u
-    v = qap.u
-    w = qap.v
-    t = qap.t
-    m = qap.m
-    n = qap.n
-    l = qap.l
+    p, G, H = ctx
+    alpha, beta, delta, x = trapdoor
+    u, v, w, t = qap.polynomials()
+    m, n, l = qap.dimensions()
 
     srs_s = [
         delta * G,                                              # δ * G
@@ -113,15 +123,16 @@ def generate_srs_s(ctx, tau, qap):
         )
     return srs_s
 
-def setup(ctx, tau, qap):
-    srs_u = generate_srs_u(ctx, tau, n=qap.n)
-    srs_s = generate_srs_s(ctx, tau, qap)
+def setup(ctx, trapdoor, qap):
+    srs_u = generate_srs_u(ctx, trapdoor, qap)
+    srs_s = generate_srs_s(ctx, trapdoor, qap)
     srs = (srs_u, srs_s)
-    return srs, tau
+    return srs, trapdoor
 
 
 # Utils
 
+from bplib import bp
 from petlib import bn
 
 def toBn(num):
@@ -136,50 +147,32 @@ def randstar(p):
     """
     return (p - 1).random()
 
-def generate_trapdoor(ctx):
-    # TODO: Provide default values for alpha, beta, delta, x
+def generate_trapdoor(ctx, alpha=None, beta=None, delta=None, x=None):
     p = ctx.p
-    alpha = randstar(p)
-    beta = randstar(p)
-    delta = randstar(p)
-    x = randstar(p)
-    tau = Tau(alpha, beta, delta, x)
-    return tau
+    args = (randstar(p) if num is None else toBn(num) 
+        for num in (alpha, beta, delta, x))
+    trapdoor = Trapdoor(*args)
+    return trapdoor
 
 CTX = namedtuple('CTX', ['p', 'G', 'H'])
 
 def create_algebraic_context():
-    from bplib import bp
-    G_T = bp.BpGroup()                      # bilinear pairing G_1 x G_2 -> G_T
-    ctx = CTX(
-        p=G_T.order(),                      # order of G_T
-        G=G_T.gen1(),                       # generator of G_1
-        H=G_T.gen2(),                       # generator of G_2
-    )
-    return ctx
-
+    group = bp.BpGroup()            # bilinear pairing G_1 x G_2 -> G_T
+    p = group.order()               # order of G_T
+    G = group.gen1()                # generator of G_1
+    H = group.gen2()                # generator of G_2
+    return CTX(p, G, H)
 
 
 if __name__ == '__main__':
     print('Snarky Ceremonies')
 
+    # Context
     ctx = create_algebraic_context()
-    tau = generate_trapdoor(ctx)                # τ = (α, β, δ, x), α, β, δ, x E (Z_p)* random
-
-    # QAP
-
-    # m=5, n=4, l=3
-
-    u = [Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0])]
-    v = [Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0])]
-    w = [Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0]), Poly([1, 0, 0, 0])]
-    t = Poly([1, 0, 0, 0, 0])
-    L = 3
-
-    qap = QAP(ctx, u, v, w, t, l=L)
+    trapdoor = generate_trapdoor(ctx)
+    qap = QAP.create_default(ctx, m=5, n=4, l=3)
 
     # SRS generation
-
-    srs, trapdoor = setup(ctx, tau, qap)
-    assert trapdoor == tau
+    srs, trapdoor = setup(ctx, trapdoor, qap)
+    assert trapdoor == trapdoor
     print(srs)
