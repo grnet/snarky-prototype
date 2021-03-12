7b222c6f9bc4966ec3c8f89c96cf16f76399e8cb
fmerg <fmerg@grnet.gr>
2021-03-12 19:01:45 +0200

    (setup): Create and apply Poly class

--- demo.py
+++ demo.py
@@ -16,6 +16,25 @@ def extract_tau(tau):
     return tau.alpha, tau.beta, tau.delta, tau.x
 
 
+class Poly(object):
+
+    def __init__(self, coeffs):
+        assert len(coeffs) >= 1
+        self.coeffs = list(map(toBn, coeffs))
+
+    def degree(self):
+        return len(self.coeffs) - 1
+
+    def coeff(self, i):
+        return self.coeffs[i]
+
+    def eval(self, x):
+        return sum(
+            map(lambda i: self.coeffs[i] * x ** i, range(0, len(self.coeffs))), 
+            bnZero()
+        )
+
+
 class QAP(object):
 
     def __init__(self, p, u, v, w, t, n, m, l):
@@ -23,31 +42,25 @@ class QAP(object):
         # Compatibility checks
 
         assert l + 1 <= m
-
-        assert len(t) == n + 1      # deg(t) = n
-
-        assert len(u) == m + 1      # u_i, 0 <= i <= m
-        assert len(v) == m + 1      # v_i, 0 <= i <= m
-        assert len(w) == m + 1      # w_i, 0 <= i <= m
-
-        for i in range(0, m + 1):       # for 0 <= i <= m
-            assert len(u[i]) == n       # deg(u_i) = n - 1
-            assert len(v[i]) == n       # deg(v_i) = n - 1
-            assert len(w[i]) == n       # deg(w_i) = n - 1
+        assert len(u) == m + 1
+        assert len(v) == m + 1
+        assert len(w) == m + 1
+        assert t.degree() == n
+        for i in range(0, m + 1):
+            assert u[i].degree() == n - 1
+            assert v[i].degree() == n - 1
+            assert w[i].degree() == n - 1
 
         # Set parameters
 
-        self.p = p      # group order
-
-        self.u = u      # u[i][j]: coefficient of X ^ j in u_i
-        self.v = v      # v[i][j]: coefficient of X ^ j in v_i
-        self.w = w      # w[i][j]: coefficient of X ^ j in w_i
-        
-        self.t = t      # t[k]: coefficient of X ^ k in t
-
-        self.n = n      # 0 <= j <= n - 1, 0 <= k <= n
-        self.m = m      # 0 <= i <= m
-        self.l = l      # 0 <= l <= m - 1 arbitrary
+        self.p = p
+        self.u = u
+        self.v = v
+        self.w = w
+        self.t = t
+        self.n = n
+        self.m = m
+        self.l = l
 
 
 def generate_srs_u(tau, G, H, n):
@@ -73,21 +86,26 @@ def generate_srs_s(tau, G, H, qap):
     u = qap.u
     v = qap.u
     w = qap.v
-    u = qap.w
+    t = qap.t
     m = qap.m
     n = qap.n
     l = qap.l
 
     srs_s = [
-        delta * G,                              # δ * G
-        delta * H,                              # δ * Η
+        delta * G,                                              # δ * G
+        delta * H,                                              # δ * Η
+        [],
+        [],
     ]
-    for i in range(l + 1, m + 1):               # l + 1 <= i <= m
-        # TODO: Implement
-        pass
-    for i in range(0, n - 1):                   # 0 <= i <= n - 2
-        # TODO: Implement
-        pass
+
+    for i in range(l + 1, m + 1):                                                                       # l + 1 <= i <= m
+        srs_s[2].append(
+            ((beta * u[i].eval(x) + alpha * v[i].eval(x) + w[i].eval(x)) * delta.mod_inverse(p)) * G    # ((β * u_i(x) + α * v_i(x) + w_i(x)) / δ) * G
+        )
+    for i in range(0, n - 1):                                                                           # 0 <= i <= n - 2
+        srs_s[3].append(
+            (x ** i * t.eval(x) * delta.mod_inverse(p)) * G                                             # ((x ^ i * t(x)) / δ) * G
+        )
     return srs_s
 
 def setup(tau):
@@ -95,6 +113,14 @@ def setup(tau):
 
 
 # Utils
+
+from petlib import bn
+
+def toBn(num):
+    return bn.Bn.from_decimal(str(num))
+
+def bnZero():
+    return toBn(0)
 
 def mk_rand(p):
     """
@@ -134,13 +160,13 @@ if __name__ == '__main__':
     N = 4   # 4 = N degree of t, 3 = N - 1 degree of u_i, v_i, w_i, 0 <= i <=5
     L = 3   # l + 1 <= m
 
-    u = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    v = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    w = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    t = [0, 0, 0, 0, 0]
+    u = [Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0])]
+    v = [Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0])]
+    w = [Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0]), Poly([0, 0, 0, 0])]
+    t = Poly([0, 0, 0, 0, 0])
 
     qap = QAP(p, u, v, w, t, n=N, m=M, l=L)
 
     srs_u = generate_srs_u(tau, G, H, n=N)
     srs_s = generate_srs_s(tau, G, H, qap)
-
+    print(srs_s)
