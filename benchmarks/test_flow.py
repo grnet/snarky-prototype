b86ab61c53f3a07336c23ca46b074c3d2962c03a
fmerg <fmerg@grnet.gr>
2021-03-24 22:28:53 +0200

    Expand benchmarks

--- benchmarks/test_flow.py
+++ benchmarks/test_flow.py
@@ -6,20 +6,46 @@ from snarky_ceremonies.flow import setup as _setup
 from snarky_ceremonies.flow import update
 from snarky_ceremonies.flow import verify
 
-def run_flow():
+# TODO: Include iterations and rounds in arguments?
+
+
+def test_qap_creation(benchmark):
+    m, n, l = 5, 4, 3
+    ctx = create_context()
+    benchmark.pedantic(QAP.create_default, args=(ctx, m, n, l))
+
+def test_setup(benchmark):
+    m, n, l = 5, 4, 3
+    ctx = create_context()
+    trapdoor = generate_trapdoor(ctx, 1, 1, 1, 1)
+    qap = QAP.create_default(ctx, m, n, l)
+    benchmark.pedantic(_setup, args=(ctx, trapdoor, qap))
+
+def update_context(m, n, l):
+    ctx = create_context()
+    trapdoor = generate_trapdoor(ctx, 1, 1, 1, 1)
+    qap = QAP.create_default(ctx, m, n, l)
+    srs, trapdoor = _setup(ctx, trapdoor, qap)
+    srs, rho = update(ctx, qap, 1, srs)
+    return ctx, qap, srs, rho
+
+def test_phase_1(benchmark):
+    ctx, qap, srs, _ = update_context(5, 4, 3)
+    benchmark.pedantic(update, args=(ctx, qap, 1, srs))
+
+def test_phase_2(benchmark):
+    ctx, qap, srs, _ = update_context(5, 4, 3)
+    benchmark.pedantic(update, args=(ctx, qap, 2, srs))
+
+def test_verification(benchmark):
+    phases = [1, 1, 1, 2, 2]
     m, n, l = 5, 4, 3
     ctx = create_context()
     trapdoor = generate_trapdoor(ctx, 1, 1, 1, 1)
     qap = QAP.create_default(ctx, m, n, l)
     srs, trapdoor = _setup(ctx, trapdoor, qap)
     Q = [[], []]
-    for phase in [1, 1, 1, 2, 2]:
+    for phase in phases:
         srs, rho = update(ctx, qap, phase, srs)
-        if phase == 1:
-            Q[0].append(rho)
-        else:
-            Q[1].append(rho)
-    assert verify(ctx, qap, srs, Q)
-
-def test_run_flow(benchmark):
-    benchmark(run_flow)
+        Q[0 if phase == 1 else 1].append(rho)
+    benchmark.pedantic(verify, args=(ctx, qap, srs, Q))
